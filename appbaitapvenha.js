74bc0fcd3d4aa489463690e7adcdcd56087ff6c5
nguyentruongan280704 <110025394+nguyentruongan280704@users.noreply.github.com>
2022-07-28 23:47:29 +0700

    Add files via upload

---
+++ appbaitapvenha.js
@@ -0,0 +1,29 @@
+//  bài 4
+// let bankinh = prompt("nhập");
+//  alert(2 *bankinh*Math.PI);
+
+// bai 1
+// let a = prompt("vatly: ");
+// let b = prompt("hoahoc: ");
+// let c = prompt("sinhhoc: ");
+// let tong = Number(a) + Number(b) + Number(c);
+// let dtb = tong  /3;
+// alert(`tong la: ${tong} va diem  trung binh: ${dtb}`);
+ 
+// bai 2
+// let celsius = prompt("do c");
+// let Fahrenheit = celsius * 1.8 + 32;
+// alert(Fahrenheit);
+
+
+//bai 3 pir2
+// let bankinh = prompt("nhap ban kinh:");
+// let S = Math.PI*bankinh*bankinh;
+// alert(S);
+
+
+
+
+
+
+
